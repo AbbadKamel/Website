806652d8b2c976137c79edf8635d3a16a6026642
oubrahimabbad <kameloubrahimabbad19@gmail.com>
2021-01-01 16:00:15 +0100

    Update phone.php

--- phone.php
+++ phone.php
@@ -8,7 +8,7 @@ require_once "Model/AccountStorageMySQL.php";
 require_once "Model/PhoneStorageMySQL.php";
 
 try {
-	$bd = new PDO("mysql:host=mysql.info.unicaen.fr;port=3306;dbname=21911536_0;charset=utf8", "21911536", "nush5vo3Foegaelu");
+	$bd = new PDO("mysql:host=mysql.info.unicaen.fr;port=3306;dbname=21911536_0;charset=utf8", "***", "***");     // i did edit it for security reasons
 } catch (Exception $e) {
 	echo "ERROR YOUR DON'T HAVE ACCESS OR SOMETHING WENT WRONG !!!!";
 }
